fc6e0550c4a8041b95552ad16c24412e4d1128d1
thwahir <thwahirpvmohd@gmail.com>
2024-07-19 12:11:49 +0530

    iam forget to add host in database

--- earc/earc/settings.py
+++ earc/earc/settings.py
@@ -98,12 +98,12 @@ DATABASES = {
         'NAME': 'mobilearc',
         'USER': 'thwahir',
         'PASSWORD': '989577',
-        'HOST' : '',
+        'HOST' : '13.60.25.71',
         'PORT': '5432'
     }
 }
 
-# 13.60.25.71
+
 
 
 
